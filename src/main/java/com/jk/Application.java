36b531ed0ce0c3dca8187ab714fc74d83c368dba
xuheng <492723592@qq.com>
2017-10-22 00:28:36 +0800

    55555555511111111111111111

--- src/main/java/com/jk/Application.java
+++ src/main/java/com/jk/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	//sidiuhfishdfhsdkjshdkfhshdfkshdfshkdfskdfhsgkuj
 	//aamakmkmkkmkmk
+	//knkjkjnkkjb444444444444444444444444444444444444444444
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
